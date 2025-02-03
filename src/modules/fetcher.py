03fa28d7a4de2de27ad5f28c0ef77572bc3dc721
Dmitry <wind.crazy125@gmail.com>
2025-02-03 13:58:24 +0300

    Upd: fetcher

--- src/modules/fetcher.py
+++ src/modules/fetcher.py
@@ -12,23 +12,27 @@ class SeleniumFetcher:
     def __init__(self, wait_time=3):
         self.wait_time = wait_time
         self.chrome_options = Options()
-        self.chrome_options.add_argument("--headless")  # Запуск в фоновом режиме
+        self.chrome_options.add_argument("--headless")  # Запуск без графического интерфейса
         self.chrome_options.add_argument("--disable-gpu")
+        self.chrome_options.add_argument("--no-sandbox")
+        self.chrome_options.add_argument("--disable-dev-shm-usage")
 
     def fetch_html(self, url):
         """Метод для получения HTML контента с указанного URL."""
         logger.info(f"Fetching HTML content from URL: {url}")
-        
-        # Инициализация драйвера
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=self.chrome_options)
-        
+
         try:
+            # Инициализация драйвера
+            driver_path = ChromeDriverManager().install()
+            service = ChromeService(driver_path)
+            driver = webdriver.Chrome(service=service, options=self.chrome_options)
+
             # Открываем страницу
             driver.get(url)
-            
-            # Ожидание для полной загрузки страницы
+
+            # Ожидание загрузки страницы
             time.sleep(self.wait_time)
-            
+
             # Получаем HTML
             html = driver.page_source
         except Exception as e:
