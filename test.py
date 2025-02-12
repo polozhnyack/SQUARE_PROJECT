9824bd215525bb72f3f0775b563b27ddcfe9ef2f
Dmitry <wind.crazy125@gmail.com>
2025-02-12 12:22:12 +0300

    del

---
+++ test.py
@@ -0,0 +1,10 @@
+def to_fraktur(text):
+    fraktur_map = str.maketrans(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+        "𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝕌𝕍𝕲𝕏𝕐𝕫"
+        "𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟"
+    )
+    return text.translate(fraktur_map)
+
+print(to_fraktur("Hi sweeties"))  
+
